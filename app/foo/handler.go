e61a83721d1acb8fdbbb69cb9fa34af8d76ad0a2
Valerii Lysenko <vallyscode@gmail.com>
2022-04-26 07:36:13 +0000

    updated template & added lambda functions

---
+++ app/foo/handler.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(ctx context.Context, event events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "{ \"message\": \"foo\" }",
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
---
+++ app/go.mod
@@ -0,0 +1,5 @@
+require github.com/aws/aws-lambda-go v1.31.1
+
+module app
+
+go 1.18
